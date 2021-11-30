959102efc40b8e24f128773dd75f586e2b0cedf9
sergeiaf <sergeiaf26@gmail.com>
2021-11-30 06:51:43 +0300

    test tutor plugin

--- src/wsgi.py
+++ src/wsgi.py
@@ -8,6 +8,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.config.local")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.config.production")
 
 application = get_wsgi_application()
